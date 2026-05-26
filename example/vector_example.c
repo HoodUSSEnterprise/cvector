cf2897a195b6052970a9f9f8da2c9e2dbd8db809
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-05-26 08:58:35 +0800

    add vector usage demo

---
+++ example/vector_example.c
@@ -0,0 +1,38 @@
+// #define DEBUG 1
+#include "vector/vector.h"
+
+int main(void)
+{
+    Complex v1 = {3, 4};
+    Complex v2 = {5, 6};
+    Complex v3 = {7, 8};
+    Vector2c *v4 = create_vec2(v1, v2);
+    Vector2c *v5 = create_vec2(v1, v3);
+    Vector2c *v6 = add_vec(v4, v5);
+    Vector2i *v7 = create_vec2(3, 6);
+    Vector2c *v8 = add_vec(v4, v7);
+    print_vec(v4);
+    print_vec(v6);
+    print_vec(v8);
+    double norm = norm_vec(v4, 2);
+    printf("%lf\n", norm);
+    Complex data[5] = {v1, v2, v3, v2, v3};
+    Vectorc *v = create_vec(5, data);
+    // Complex remove[2] = {v1, v2};
+    int da[5] = {1, 2, 3, 4, 5};
+
+    int re[2] = {1, 2};
+    Vectori *vint = create_vec(5, da);
+    Vectori *vone = remove_elem(vint, 1);
+    Vectori *varr = remove_arr(vint, re, 2);
+    print_vec(v);
+    print_vec(vone);
+    print_vec(varr);
+    Vectori *vreplace = replace_elem(vint, 2, 5);
+    print_vec(vreplace);
+    int old_arr[2] = {1, 2};
+    int new_arr[2] = {3, 4};
+    Vectori *vreplacearr = replace_arr(vint, old_arr, new_arr, 2);
+    print_vec(vreplacearr);
+    return 0;
+}
--- include/vector/vector.h
+++ include/vector/vector.h
@@ -2,7 +2,12 @@
 #define VECTOR_H
 
 #include "vector/base_vector.h"
+#include "vector/operator_vector.h"
+#include "vector/print_vector.h"
 #include "vector/create_vector.h"
 #include "vector/free_vector.h"
+#include "vector/find_vector.h"
+#include "vector/replace_vector.h"
+#include "vector/remove_vector.h"
 
 #endif // VECTOR_H
