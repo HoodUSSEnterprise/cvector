6531203a265274061b6519de6459d9ca8a896010
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-05-29 16:38:59 +0800

    fix:some bug in determinant

--- include/matrix/subdeterminant_matrix.h
+++ include/matrix/subdeterminant_matrix.h
@@ -3,7 +3,11 @@
 
 #include "matrix/base_matrix.h"
 #include "complex/operator_complex.h"
-#include "matrix/determinant_matrix.h"
+
+int det_matrixi(MatrixI *m);
+float det_matrixf(MatrixF *m);
+double det_matrixd(MatrixD *m);
+Complex det_matrixc(MatrixC *m);
 
 // ========== 余子式（移除指定行和列后的子矩阵） ==========
 
@@ -32,5 +36,11 @@ MatrixC *cofactor_matrixc(MatrixC *m);
     MatrixF *: cofactor_matrixf,         \
     MatrixD *: cofactor_matrixd,         \
     MatrixC *: cofactor_matrixc)(m)
+
+#define det_matrix(m) _Generic((m), \
+    MatrixI *: det_matrixi,         \
+    MatrixF *: det_matrixf,         \
+    MatrixD *: det_matrixd,         \
+    MatrixC *: det_matrixc)(m)
 
 #endif // SUBDETERMINANT_MATRIX_H
