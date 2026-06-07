4c558119f86fc750019fa8d578b383b5220401f2
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:32:12 +0800

    add unordered_multiset contains and count

---
+++ include/unordered_multiset/find_unordered_multiset.h
@@ -0,0 +1,9 @@
+#ifndef FIND_UNORDERED_MULTISET_H
+#define FIND_UNORDERED_MULTISET_H
+
+#include "unordered_multiset/base_unordered_multiset.h"
+
+int unordered_multiset_contains(UnorderedMultiset *set, int key);
+int unordered_multiset_count(UnorderedMultiset *set, int key);
+
+#endif
---
+++ src/unordered_multiset/find_unordered_multiset.c
@@ -0,0 +1,32 @@
+#include "unordered_multiset/find_unordered_multiset.h"
+
+static int hash_key(int key, int capacity)
+{
+    return abs(key) % capacity;
+}
+
+int unordered_multiset_contains(UnorderedMultiset *set, int key)
+{
+    int idx = hash_key(key, set->capacity);
+
+    UnorderedMultisetNode *cur = set->buckets[idx];
+    while (cur)
+    {
+        if (cur->key == key) return 1;
+        cur = cur->next;
+    }
+    return 0;
+}
+
+int unordered_multiset_count(UnorderedMultiset *set, int key)
+{
+    int idx = hash_key(key, set->capacity);
+
+    UnorderedMultisetNode *cur = set->buckets[idx];
+    while (cur)
+    {
+        if (cur->key == key) return cur->count;
+        cur = cur->next;
+    }
+    return 0;
+}
