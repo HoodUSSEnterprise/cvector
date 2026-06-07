7746cdf7084fcbeb25ba7e3cbcfca3bf6d8b6936
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:03:46 +0800

    add set umbrella header

---
+++ include/set/set.h
@@ -0,0 +1,12 @@
+#ifndef SET_H
+#define SET_H
+
+#include "set/base_set.h"
+#include "set/create_set.h"
+#include "set/insert_set.h"
+#include "set/delete_set.h"
+#include "set/find_set.h"
+#include "set/free_set.h"
+#include "set/operator_set.h"
+
+#endif
