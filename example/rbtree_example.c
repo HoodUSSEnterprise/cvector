c71dd2e6129029c93b27f08f658ac872b667f470
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:49:04 +0800

    add comments to rbtree_example.c

--- example/rbtree_example.c
+++ example/rbtree_example.c
@@ -4,6 +4,7 @@ int main(void)
 {
     RBTree *tree = create_rbtree();
 
+    /* 插入一组测试数据 */
     int vals[] = {7, 3, 18, 10, 22, 8, 11, 26, 2, 6, 13};
     int n = sizeof(vals) / sizeof(vals[0]);
 
@@ -15,19 +16,23 @@ int main(void)
     }
     printf("\n\n");
 
+    /* 三种遍历方式打印 */
     rbtree_print_preorder(tree);
     rbtree_print_inorder(tree);
     rbtree_print_postorder(tree);
     printf("\n");
 
+    /* 基本信息 */
     printf("Size: %d\n", rbtree_size(tree));
     printf("Height: %d\n", rbtree_height(tree));
     printf("Min: %d\n", rbtree_min(tree));
     printf("Max: %d\n", rbtree_max(tree));
 
+    /* 查找测试 */
     printf("\nContains 10? %s\n", rbtree_contains(tree, 10) ? "yes" : "no");
     printf("Contains 99? %s\n", rbtree_contains(tree, 99) ? "yes" : "no");
 
+    /* 删除测试 */
     printf("\nDeleting 18, 3, 10\n");
     rbtree_delete(tree, 18);
     rbtree_delete(tree, 3);
@@ -35,6 +40,7 @@ int main(void)
     rbtree_print_inorder(tree);
     printf("Size after delete: %d\n\n", rbtree_size(tree));
 
+    /* 删除后再插入 */
     printf("Inserting 1, 4, 5, 20:\n");
     rbtree_insert(tree, 1);
     rbtree_insert(tree, 4);
