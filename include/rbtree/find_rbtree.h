8d7a97b7cb1aaa1e59b80bf668c52866e7bddd23
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:48:18 +0800

    add comments to find_rbtree.h

--- include/rbtree/find_rbtree.h
+++ include/rbtree/find_rbtree.h
@@ -4,7 +4,9 @@
 #include "rbtree/base_rbtree.h"
 #include <stdbool.h>
 
+/* 在树中查找 key，返回结点指针；未找到返回 NULL */
 RBNode *rbtree_find(RBTree *tree, int key);
+/* 判断 key 是否存在于树中 */
 bool rbtree_contains(RBTree *tree, int key);
 
 #endif
