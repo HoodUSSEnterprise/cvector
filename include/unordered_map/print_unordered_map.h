341d016c6c07a696cdea2af4f58e935fff318af2
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:29 +0800

    add unordered_map print

---
+++ include/unordered_map/print_unordered_map.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_UNORDERED_MAP_H
+#define PRINT_UNORDERED_MAP_H
+
+#include "unordered_map/base_unordered_map.h"
+
+void unordered_map_print(UnorderedMap *map);
+
+#endif
---
+++ src/unordered_map/print_unordered_map.c
@@ -0,0 +1,19 @@
+#include "unordered_map/print_unordered_map.h"
+
+void unordered_map_print(UnorderedMap *map)
+{
+    printf("{");
+    int first = 1;
+    for (int i = 0; i < map->capacity; i++)
+    {
+        UnorderedMapNode *cur = map->buckets[i];
+        while (cur)
+        {
+            if (!first) printf(", ");
+            printf("%d: %d", cur->key, cur->value);
+            first = 0;
+            cur = cur->next;
+        }
+    }
+    printf("}\n");
+}
