1a613aaa04f654777b8ad7ec981113ec90834b1e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:15:07 +0800

    add forward_list front, size, is_empty

---
+++ include/forwardlist/operator_forwardlist.h
@@ -0,0 +1,10 @@
+#ifndef OPERATOR_FORWARDLIST_H
+#define OPERATOR_FORWARDLIST_H
+
+#include "forwardlist/base_forwardlist.h"
+
+int front_forwardlist(ForwardList *list);
+int size_forwardlist(ForwardList *list);
+int is_empty_forwardlist(ForwardList *list);
+
+#endif
---
+++ src/forwardlist/operator_forwardlist.c
@@ -0,0 +1,17 @@
+#include "forwardlist/operator_forwardlist.h"
+
+int front_forwardlist(ForwardList *list)
+{
+    if (!list->head) return 0;
+    return list->head->data;
+}
+
+int size_forwardlist(ForwardList *list)
+{
+    return list->size;
+}
+
+int is_empty_forwardlist(ForwardList *list)
+{
+    return list->size == 0;
+}
