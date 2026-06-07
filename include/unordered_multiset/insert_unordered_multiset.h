300998a40c53a978a61c3d34cf643c9dcbb4e510
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:32:11 +0800

    add unordered_multiset insert

---
+++ include/unordered_multiset/insert_unordered_multiset.h
@@ -0,0 +1,8 @@
+#ifndef INSERT_UNORDERED_MULTISET_H
+#define INSERT_UNORDERED_MULTISET_H
+
+#include "unordered_multiset/base_unordered_multiset.h"
+
+void unordered_multiset_insert(UnorderedMultiset *set, int key);
+
+#endif
---
+++ src/unordered_multiset/insert_unordered_multiset.c
@@ -0,0 +1,32 @@
+#include "unordered_multiset/insert_unordered_multiset.h"
+
+static int hash_key(int key, int capacity)
+{
+    return abs(key) % capacity;
+}
+
+void unordered_multiset_insert(UnorderedMultiset *set, int key)
+{
+    int idx = hash_key(key, set->capacity);
+
+    UnorderedMultisetNode *cur = set->buckets[idx];
+    while (cur)
+    {
+        if (cur->key == key)
+        {
+            cur->count++;
+            set->size++;
+            return;
+        }
+        cur = cur->next;
+    }
+
+    UnorderedMultisetNode *node = (UnorderedMultisetNode *)malloc(sizeof(UnorderedMultisetNode));
+    if (!node) return;
+    node->key = key;
+    node->count = 1;
+    node->next = set->buckets[idx];
+    set->buckets[idx] = node;
+    set->size++;
+    set->unique++;
+}
