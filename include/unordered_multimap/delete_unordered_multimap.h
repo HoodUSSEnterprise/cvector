d2f9d38f7224df174cdafd5955016f95a1dec574
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:33:21 +0800

    add unordered_multimap delete

---
+++ include/unordered_multimap/delete_unordered_multimap.h
@@ -0,0 +1,8 @@
+#ifndef DELETE_UNORDERED_MULTIMAP_H
+#define DELETE_UNORDERED_MULTIMAP_H
+
+#include "unordered_multimap/base_unordered_multimap.h"
+
+void unordered_multimap_delete(UnorderedMultimap *map, int key);
+
+#endif
---
+++ src/unordered_multimap/delete_unordered_multimap.c
@@ -0,0 +1,30 @@
+#include "unordered_multimap/delete_unordered_multimap.h"
+
+static int hash_key(int key, int capacity)
+{
+    return abs(key) % capacity;
+}
+
+void unordered_multimap_delete(UnorderedMultimap *map, int key)
+{
+    int idx = hash_key(key, map->capacity);
+
+    UnorderedMultimapNode *cur = map->buckets[idx];
+    UnorderedMultimapNode *prev = NULL;
+
+    while (cur)
+    {
+        if (cur->key == key)
+        {
+            if (prev)
+                prev->next = cur->next;
+            else
+                map->buckets[idx] = cur->next;
+            free(cur);
+            map->size--;
+            return;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+}
