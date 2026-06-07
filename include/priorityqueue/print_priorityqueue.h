15f387fabbddb8ed5cef5a79852b2a0ebd271855
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:16:48 +0800

    add priority_queue print

---
+++ include/priorityqueue/print_priorityqueue.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_PRIORITYQUEUE_H
+#define PRINT_PRIORITYQUEUE_H
+
+#include "priorityqueue/base_priorityqueue.h"
+
+void print_priorityqueue(PriorityQueue *pq);
+
+#endif
---
+++ src/priorityqueue/print_priorityqueue.c
@@ -0,0 +1,12 @@
+#include "priorityqueue/print_priorityqueue.h"
+
+void print_priorityqueue(PriorityQueue *pq)
+{
+    printf("[");
+    for (int i = 0; i < pq->size; i++)
+    {
+        if (i > 0) printf(", ");
+        printf("%d", pq->data[i]);
+    }
+    printf("]\n");
+}
