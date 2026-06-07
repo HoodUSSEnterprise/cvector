b125d3fed5b1d3caa560916c8d65628e3c6127c8
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:37 +0800

    add unordered_set free and clear

---
+++ include/unordered_set/free_unordered_set.h
@@ -0,0 +1,9 @@
+#ifndef FREE_UNORDERED_SET_H
+#define FREE_UNORDERED_SET_H
+
+#include "unordered_set/base_unordered_set.h"
+
+void clear_unordered_set(UnorderedSet *set);
+void free_unordered_set(UnorderedSet *set);
+
+#endif
---
+++ src/unordered_set/free_unordered_set.c
@@ -0,0 +1,25 @@
+#include "unordered_set/free_unordered_set.h"
+
+void clear_unordered_set(UnorderedSet *set)
+{
+    for (int i = 0; i < set->capacity; i++)
+    {
+        UnorderedSetNode *cur = set->buckets[i];
+        while (cur)
+        {
+            UnorderedSetNode *tmp = cur;
+            cur = cur->next;
+            free(tmp);
+        }
+        set->buckets[i] = NULL;
+    }
+    set->size = 0;
+}
+
+void free_unordered_set(UnorderedSet *set)
+{
+    if (!set) return;
+    clear_unordered_set(set);
+    free(set->buckets);
+    free(set);
+}
