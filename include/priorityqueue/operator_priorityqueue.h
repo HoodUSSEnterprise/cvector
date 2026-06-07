bfb6ccae13b94a36f21de37ddecee7c0c0aa1313
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:16:48 +0800

    add priority_queue top, size, is_empty

---
+++ include/priorityqueue/operator_priorityqueue.h
@@ -0,0 +1,10 @@
+#ifndef OPERATOR_PRIORITYQUEUE_H
+#define OPERATOR_PRIORITYQUEUE_H
+
+#include "priorityqueue/base_priorityqueue.h"
+
+int top_priorityqueue(PriorityQueue *pq);
+int size_priorityqueue(PriorityQueue *pq);
+int is_empty_priorityqueue(PriorityQueue *pq);
+
+#endif
---
+++ src/priorityqueue/operator_priorityqueue.c
@@ -0,0 +1,17 @@
+#include "priorityqueue/operator_priorityqueue.h"
+
+int top_priorityqueue(PriorityQueue *pq)
+{
+    if (pq->size == 0) return 0;
+    return pq->data[0];
+}
+
+int size_priorityqueue(PriorityQueue *pq)
+{
+    return pq->size;
+}
+
+int is_empty_priorityqueue(PriorityQueue *pq)
+{
+    return pq->size == 0;
+}
