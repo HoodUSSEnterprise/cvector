acb987658a5fd5ce5e8d6242434ba27c88c2cf32
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:26 +0800

    add unordered_map create

---
+++ include/unordered_map/create_unordered_map.h
@@ -0,0 +1,8 @@
+#ifndef CREATE_UNORDERED_MAP_H
+#define CREATE_UNORDERED_MAP_H
+
+#include "unordered_map/base_unordered_map.h"
+
+UnorderedMap *create_unordered_map(void);
+
+#endif
---
+++ src/unordered_map/create_unordered_map.c
@@ -0,0 +1,16 @@
+#include "unordered_map/create_unordered_map.h"
+
+UnorderedMap *create_unordered_map(void)
+{
+    UnorderedMap *map = (UnorderedMap *)malloc(sizeof(UnorderedMap));
+    if (!map) return NULL;
+    map->capacity = UNORDERED_MAP_INITIAL_CAPACITY;
+    map->size = 0;
+    map->buckets = (UnorderedMapNode **)calloc(map->capacity, sizeof(UnorderedMapNode *));
+    if (!map->buckets)
+    {
+        free(map);
+        return NULL;
+    }
+    return map;
+}
