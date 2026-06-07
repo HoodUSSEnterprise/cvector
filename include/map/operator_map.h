bb21aa866874c6c2e15bef427465fa3033d1ecbb
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:04:19 +0800

    add operator_map header

---
+++ include/map/operator_map.h
@@ -0,0 +1,11 @@
+#ifndef OPERATOR_MAP_H
+#define OPERATOR_MAP_H
+
+#include "map/base_map.h"
+#include <stdbool.h>
+
+int map_size(Map *map);
+bool map_is_empty(Map *map);
+void map_print(Map *map);
+
+#endif
