22e44dddd7a1a824ae9c690600cf63a808bb0036
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 12:33:45 +0800

    fix:bug

--- example/stack_example.c
+++ example/stack_example.c
@@ -1,61 +1,23 @@
-#include "stack.h"
+#include "stack/stack.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// 简单函数1：整数入栈出栈
 void test_int()
 {
     printf("=== 整数测试 ===\n");
     Stack *s = create_stack();
 
     int a = 10, b = 20, c = 30;
-    push(s, &a);
-    push(s, &b);
-    push(s, &c);
+    push(s, a);
+    push(s, b);
+    push(s, c);
 
-    int *p;
-    pop(s, (void **)&p);
-    printf("出栈: %d\n", *p);
-    pop(s, (void **)&p);
-    printf("出栈: %d\n", *p);
-
-    free_stack(s);
-}
-
-// 简单函数2：字符串入栈出栈
-void test_string()
-{
-    printf("\n=== 字符串测试 ===\n");
-    Stack *s = create_stack();
-
-    char *str1 = "Hello";
-    char *str2 = "World";
-    push(s, str1);
-    push(s, str2);
-
-    char *str;
-    pop(s, (void **)&str);
-    printf("出栈: %s\n", str);
-    pop(s, (void **)&str);
-    printf("出栈: %s\n", str);
-
-    free_stack(s);
-}
-
-// 简单函数3：查看栈顶
-void test_top()
-{
-    printf("\n=== 查看栈顶测试 ===\n");
-    Stack *s = create_stack();
-
-    int x = 100;
-    push(s, &x);
-
-    int *p;
-    top(s, (void **)&p);
-    printf("栈顶: %d\n", *p);
-    printf("栈大小: %d\n", get_stack_size(s));
+    int p = 0;
+    pop(s, &p);
+    printf("出栈: %d\n", p);
+    pop(s, &p);
+    printf("出栈: %d\n", p);
 
     free_stack(s);
 }
@@ -64,7 +26,5 @@ int main()
 {
     system("chcp 65001");
     test_int();
-    test_string();
-    test_top();
     return 0;
 }
