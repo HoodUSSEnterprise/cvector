8fab146514b3bd2737964984437562cb528c8789
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:28 +0800

    add unordered_map free and clear

---
+++ include/unordered_map/free_unordered_map.h
@@ -0,0 +1,9 @@
+#ifndef FREE_UNORDERED_MAP_H
+#define FREE_UNORDERED_MAP_H
+
+#include "unordered_map/base_unordered_map.h"
+
+void clear_unordered_map(UnorderedMap *map);
+void free_unordered_map(UnorderedMap *map);
+
+#endif
---
+++ src/unordered_map/free_unordered_map.c
@@ -0,0 +1,25 @@
+#include "unordered_map/free_unordered_map.h"
+
+void clear_unordered_map(UnorderedMap *map)
+{
+    for (int i = 0; i < map->capacity; i++)
+    {
+        UnorderedMapNode *cur = map->buckets[i];
+        while (cur)
+        {
+            UnorderedMapNode *tmp = cur;
+            cur = cur->next;
+            free(tmp);
+        }
+        map->buckets[i] = NULL;
+    }
+    map->size = 0;
+}
+
+void free_unordered_map(UnorderedMap *map)
+{
+    if (!map) return;
+    clear_unordered_map(map);
+    free(map->buckets);
+    free(map);
+}
