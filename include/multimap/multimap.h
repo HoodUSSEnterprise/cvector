bb7769217c2a18067a06f9bbc6e9191bedee7290
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:37:10 +0800

    add multimap aggregate header

---
+++ include/multimap/multimap.h
@@ -0,0 +1,13 @@
+#ifndef MULTIMAP_H
+#define MULTIMAP_H
+
+#include "multimap/base_multimap.h"
+#include "multimap/create_multimap.h"
+#include "multimap/insert_multimap.h"
+#include "multimap/delete_multimap.h"
+#include "multimap/find_multimap.h"
+#include "multimap/free_multimap.h"
+#include "multimap/operator_multimap.h"
+#include "multimap/print_multimap.h"
+
+#endif
