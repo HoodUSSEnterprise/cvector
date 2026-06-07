21a0c0916779ac8b0627a12fa6d02f9fc9d60a5a
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:06:41 +0800

    fix set_insert to ignore duplicates

--- src/set/insert_set.c
+++ src/set/insert_set.c
@@ -1,6 +1,8 @@
 #include "set/insert_set.h"
+#include "rbtree/rbtree.h"
 
 void set_insert(Set *set, int key)
 {
+    if (rbtree_contains(set->tree, key)) return;
     rbtree_insert(set->tree, key);
 }
