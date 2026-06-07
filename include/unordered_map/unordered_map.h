f94ab82a80be27fd4d1bfb096d8c5ec82262f79f
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:29 +0800

    add unordered_map aggregate header

---
+++ include/unordered_map/unordered_map.h
@@ -0,0 +1,13 @@
+#ifndef UNORDERED_MAP_H
+#define UNORDERED_MAP_H
+
+#include "unordered_map/base_unordered_map.h"
+#include "unordered_map/create_unordered_map.h"
+#include "unordered_map/insert_unordered_map.h"
+#include "unordered_map/delete_unordered_map.h"
+#include "unordered_map/find_unordered_map.h"
+#include "unordered_map/free_unordered_map.h"
+#include "unordered_map/operator_unordered_map.h"
+#include "unordered_map/print_unordered_map.h"
+
+#endif
