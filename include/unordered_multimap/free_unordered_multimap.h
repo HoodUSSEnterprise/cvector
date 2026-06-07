8ae939aeb172674d2e44eb7e7e0dddfd462299ff
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:33:22 +0800

    add unordered_multimap free and clear

---
+++ include/unordered_multimap/free_unordered_multimap.h
@@ -0,0 +1,9 @@
+#ifndef FREE_UNORDERED_MULTIMAP_H
+#define FREE_UNORDERED_MULTIMAP_H
+
+#include "unordered_multimap/base_unordered_multimap.h"
+
+void clear_unordered_multimap(UnorderedMultimap *map);
+void free_unordered_multimap(UnorderedMultimap *map);
+
+#endif
---
+++ src/unordered_multimap/free_unordered_multimap.c
@@ -0,0 +1,25 @@
+#include "unordered_multimap/free_unordered_multimap.h"
+
+void clear_unordered_multimap(UnorderedMultimap *map)
+{
+    for (int i = 0; i < map->capacity; i++)
+    {
+        UnorderedMultimapNode *cur = map->buckets[i];
+        while (cur)
+        {
+            UnorderedMultimapNode *tmp = cur;
+            cur = cur->next;
+            free(tmp);
+        }
+        map->buckets[i] = NULL;
+    }
+    map->size = 0;
+}
+
+void free_unordered_multimap(UnorderedMultimap *map)
+{
+    if (!map) return;
+    clear_unordered_multimap(map);
+    free(map->buckets);
+    free(map);
+}
