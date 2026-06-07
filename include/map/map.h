0302ec4231125a889203e43d4ecd50dc6d9afb5e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:04:19 +0800

    add map umbrella header

---
+++ include/map/map.h
@@ -0,0 +1,12 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include "map/base_map.h"
+#include "map/create_map.h"
+#include "map/insert_map.h"
+#include "map/delete_map.h"
+#include "map/find_map.h"
+#include "map/free_map.h"
+#include "map/operator_map.h"
+
+#endif
