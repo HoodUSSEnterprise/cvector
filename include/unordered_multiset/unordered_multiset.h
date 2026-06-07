7cedc6f1624856fad84cf71ae687e84b41f21dd2
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:32:13 +0800

    add unordered_multiset aggregate header

---
+++ include/unordered_multiset/unordered_multiset.h
@@ -0,0 +1,13 @@
+#ifndef UNORDERED_MULTISET_H
+#define UNORDERED_MULTISET_H
+
+#include "unordered_multiset/base_unordered_multiset.h"
+#include "unordered_multiset/create_unordered_multiset.h"
+#include "unordered_multiset/insert_unordered_multiset.h"
+#include "unordered_multiset/delete_unordered_multiset.h"
+#include "unordered_multiset/find_unordered_multiset.h"
+#include "unordered_multiset/free_unordered_multiset.h"
+#include "unordered_multiset/operator_unordered_multiset.h"
+#include "unordered_multiset/print_unordered_multiset.h"
+
+#endif
