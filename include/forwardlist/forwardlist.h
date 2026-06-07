36535853e636f2e44ff597bb4a75bd01531d9c9d
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:15:10 +0800

    add forward_list aggregate header

---
+++ include/forwardlist/forwardlist.h
@@ -0,0 +1,12 @@
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
+
+#include "forwardlist/base_forwardlist.h"
+#include "forwardlist/create_forwardlist.h"
+#include "forwardlist/push_front_forwardlist.h"
+#include "forwardlist/pop_front_forwardlist.h"
+#include "forwardlist/operator_forwardlist.h"
+#include "forwardlist/print_forwardlist.h"
+#include "forwardlist/free_forwardlist.h"
+
+#endif
