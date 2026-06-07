4d9a7770ac18955b3e6e8b04629dad3be8064c7c
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:35:25 +0800

    add rbtree count

---
+++ include/rbtree/count_rbtree.h
@@ -0,0 +1,8 @@
+#ifndef COUNT_RBTREE_H
+#define COUNT_RBTREE_H
+
+#include "rbtree/base_rbtree.h"
+
+int rbtree_count(RBTree *tree, int key);
+
+#endif
---
+++ src/rbtree/count_rbtree.c
@@ -0,0 +1,13 @@
+#include "rbtree/count_rbtree.h"
+
+static int count_nodes(RBNode *node, RBNode *nil, int key)
+{
+    if (node == nil) return 0;
+    int c = (node->key == key) ? 1 : 0;
+    return c + count_nodes(node->left, nil, key) + count_nodes(node->right, nil, key);
+}
+
+int rbtree_count(RBTree *tree, int key)
+{
+    return count_nodes(tree->root, tree->nil, key);
+}
