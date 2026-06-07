a0a16f6c070e287f3133555fa298a3dd215f65c1
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:18:38 +0800

    add unordered_set print

---
+++ include/unordered_set/print_unordered_set.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_UNORDERED_SET_H
+#define PRINT_UNORDERED_SET_H
+
+#include "unordered_set/base_unordered_set.h"
+
+void unordered_set_print(UnorderedSet *set);
+
+#endif
---
+++ src/unordered_set/print_unordered_set.c
@@ -0,0 +1,19 @@
+#include "unordered_set/print_unordered_set.h"
+
+void unordered_set_print(UnorderedSet *set)
+{
+    printf("{");
+    int first = 1;
+    for (int i = 0; i < set->capacity; i++)
+    {
+        UnorderedSetNode *cur = set->buckets[i];
+        while (cur)
+        {
+            if (!first) printf(", ");
+            printf("%d", cur->key);
+            first = 0;
+            cur = cur->next;
+        }
+    }
+    printf("}\n");
+}
