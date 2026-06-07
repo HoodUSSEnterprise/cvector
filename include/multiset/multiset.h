aa0343287a81c825506d9fdec69cb60ee7884d34
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:35:46 +0800

    add multiset aggregate header

---
+++ include/multiset/multiset.h
@@ -0,0 +1,13 @@
+#ifndef MULTISET_H
+#define MULTISET_H
+
+#include "multiset/base_multiset.h"
+#include "multiset/create_multiset.h"
+#include "multiset/insert_multiset.h"
+#include "multiset/delete_multiset.h"
+#include "multiset/find_multiset.h"
+#include "multiset/free_multiset.h"
+#include "multiset/operator_multiset.h"
+#include "multiset/print_multiset.h"
+
+#endif
