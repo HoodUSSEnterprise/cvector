a621c19188f24ac8b5f7ff0e5136b5bc1560a7cf
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:33:26 +0800

    add unordered_multimap aggregate header

---
+++ include/unordered_multimap/unordered_multimap.h
@@ -0,0 +1,13 @@
+#ifndef UNORDERED_MULTIMAP_H
+#define UNORDERED_MULTIMAP_H
+
+#include "unordered_multimap/base_unordered_multimap.h"
+#include "unordered_multimap/create_unordered_multimap.h"
+#include "unordered_multimap/insert_unordered_multimap.h"
+#include "unordered_multimap/delete_unordered_multimap.h"
+#include "unordered_multimap/find_unordered_multimap.h"
+#include "unordered_multimap/free_unordered_multimap.h"
+#include "unordered_multimap/operator_unordered_multimap.h"
+#include "unordered_multimap/print_unordered_multimap.h"
+
+#endif
