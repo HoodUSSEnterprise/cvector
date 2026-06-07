bea422c0b9e4843678680c4acb64d4f4ed4b166c
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:48:19 +0800

    add comments to operator_rbtree.h

--- include/rbtree/operator_rbtree.h
+++ include/rbtree/operator_rbtree.h
@@ -3,12 +3,16 @@
 
 #include "rbtree/base_rbtree.h"
 #include <stdbool.h>
-#include <limits.h>
 
+/* 返回树中最小键值（空树返回 0） */
 int rbtree_min(RBTree *tree);
+/* 返回树中最大键值（空树返回 0） */
 int rbtree_max(RBTree *tree);
+/* 返回树的高度（空树为 0） */
 int rbtree_height(RBTree *tree);
+/* 返回树的结点数 */
 int rbtree_size(RBTree *tree);
+/* 判断树是否为空 */
 bool rbtree_is_empty(RBTree *tree);
 
 #endif
