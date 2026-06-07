294b7ec5b00acfcd5e5f947c174c11ea4ccc6dce
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:48:55 +0800

    add comments to free_rbtree.c

--- src/rbtree/free_rbtree.c
+++ src/rbtree/free_rbtree.c
@@ -1,5 +1,6 @@
 #include "rbtree/free_rbtree.h"
 
+/* 递归后序遍历释放所有结点 */
 static void free_nodes(RBTree *tree, RBNode *x)
 {
     if (x == tree->nil)
@@ -9,10 +10,11 @@ static void free_nodes(RBTree *tree, RBNode *x)
     free(x);
 }
 
+/* 释放整棵红黑树 */
 void free_rbtree(RBTree *tree)
 {
     if (tree == NULL) return;
     free_nodes(tree, tree->root);
-    free(tree->nil);
+    free(tree->nil);  /* 释放哨兵结点 */
     free(tree);
 }
