ca73df0eaaf5ce4813fc813653f88339929adf80
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:16:49 +0800

    add priority_queue aggregate header

---
+++ include/priorityqueue/priorityqueue.h
@@ -0,0 +1,12 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+#include "priorityqueue/base_priorityqueue.h"
+#include "priorityqueue/create_priorityqueue.h"
+#include "priorityqueue/push_priorityqueue.h"
+#include "priorityqueue/pop_priorityqueue.h"
+#include "priorityqueue/operator_priorityqueue.h"
+#include "priorityqueue/print_priorityqueue.h"
+#include "priorityqueue/free_priorityqueue.h"
+
+#endif
