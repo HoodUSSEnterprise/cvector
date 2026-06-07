e6340c483dbd6ce3c80e5a2d0d03e0239151d580
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:32:10 +0800

    add unordered_multiset create

---
+++ include/unordered_multiset/create_unordered_multiset.h
@@ -0,0 +1,8 @@
+#ifndef CREATE_UNORDERED_MULTISET_H
+#define CREATE_UNORDERED_MULTISET_H
+
+#include "unordered_multiset/base_unordered_multiset.h"
+
+UnorderedMultiset *create_unordered_multiset(void);
+
+#endif
---
+++ src/unordered_multiset/create_unordered_multiset.c
@@ -0,0 +1,17 @@
+#include "unordered_multiset/create_unordered_multiset.h"
+
+UnorderedMultiset *create_unordered_multiset(void)
+{
+    UnorderedMultiset *set = (UnorderedMultiset *)malloc(sizeof(UnorderedMultiset));
+    if (!set) return NULL;
+    set->capacity = UNORDERED_MULTISET_INITIAL_CAPACITY;
+    set->size = 0;
+    set->unique = 0;
+    set->buckets = (UnorderedMultisetNode **)calloc(set->capacity, sizeof(UnorderedMultisetNode *));
+    if (!set->buckets)
+    {
+        free(set);
+        return NULL;
+    }
+    return set;
+}
