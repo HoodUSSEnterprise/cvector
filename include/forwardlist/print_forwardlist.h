f9e844dad99966e1c43d4bd6563ad35d44e0beae
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:15:07 +0800

    add forward_list print

---
+++ include/forwardlist/print_forwardlist.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_FORWARDLIST_H
+#define PRINT_FORWARDLIST_H
+
+#include "forwardlist/base_forwardlist.h"
+
+void print_forwardlist(ForwardList *list);
+
+#endif
---
+++ src/forwardlist/print_forwardlist.c
@@ -0,0 +1,12 @@
+#include "forwardlist/print_forwardlist.h"
+
+void print_forwardlist(ForwardList *list)
+{
+    printf("[");
+    for (ForwardListNode *cur = list->head; cur; cur = cur->next)
+    {
+        if (cur != list->head) printf(" -> ");
+        printf("%d", cur->data);
+    }
+    printf("]\n");
+}
