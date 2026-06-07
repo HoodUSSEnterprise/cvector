cb4dcdc75a027be0b9ad55b1d9f850a2e324b78e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:46:33 +0800

    add: rbtree umbrella header

---
+++ include/rbtree/rbtree.h
@@ -0,0 +1,13 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+
+#include "rbtree/base_rbtree.h"
+#include "rbtree/create_rbtree.h"
+#include "rbtree/insert_rbtree.h"
+#include "rbtree/delete_rbtree.h"
+#include "rbtree/find_rbtree.h"
+#include "rbtree/free_rbtree.h"
+#include "rbtree/print_rbtree.h"
+#include "rbtree/operator_rbtree.h"
+
+#endif
