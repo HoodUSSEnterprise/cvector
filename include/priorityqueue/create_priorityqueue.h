1fe64bb9592b1cbd4a6295f15d3356937aed3cf2
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:16:46 +0800

    add priority_queue create

---
+++ include/priorityqueue/create_priorityqueue.h
@@ -0,0 +1,8 @@
+#ifndef CREATE_PRIORITYQUEUE_H
+#define CREATE_PRIORITYQUEUE_H
+
+#include "priorityqueue/base_priorityqueue.h"
+
+PriorityQueue *create_priorityqueue(void);
+
+#endif
---
+++ src/priorityqueue/create_priorityqueue.c
@@ -0,0 +1,16 @@
+#include "priorityqueue/create_priorityqueue.h"
+
+PriorityQueue *create_priorityqueue(void)
+{
+    PriorityQueue *pq = (PriorityQueue *)malloc(sizeof(PriorityQueue));
+    if (!pq) return NULL;
+    pq->data = (int *)malloc(sizeof(int) * INITIAL_CAPACITY);
+    if (!pq->data)
+    {
+        free(pq);
+        return NULL;
+    }
+    pq->size = 0;
+    pq->capacity = INITIAL_CAPACITY;
+    return pq;
+}
