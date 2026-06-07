821d7f5f50d72ca481f7e1113917c30dfca3dbae
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:46:11 +0800

    add: rbtree print header

---
+++ include/rbtree/print_rbtree.h
@@ -0,0 +1,10 @@
+#ifndef PRINT_RBTREE_H
+#define PRINT_RBTREE_H
+
+#include "rbtree/base_rbtree.h"
+
+void rbtree_print_inorder(RBTree *tree);
+void rbtree_print_preorder(RBTree *tree);
+void rbtree_print_postorder(RBTree *tree);
+
+#endif
