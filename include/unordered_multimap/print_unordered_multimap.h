ff77aea4e955ce8afdc04a6c5a6b032cfc7d162e
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:33:26 +0800

    add unordered_multimap print

---
+++ include/unordered_multimap/print_unordered_multimap.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_UNORDERED_MULTIMAP_H
+#define PRINT_UNORDERED_MULTIMAP_H
+
+#include "unordered_multimap/base_unordered_multimap.h"
+
+void unordered_multimap_print(UnorderedMultimap *map);
+
+#endif
---
+++ src/unordered_multimap/print_unordered_multimap.c
@@ -0,0 +1,19 @@
+#include "unordered_multimap/print_unordered_multimap.h"
+
+void unordered_multimap_print(UnorderedMultimap *map)
+{
+    printf("{");
+    int first = 1;
+    for (int i = 0; i < map->capacity; i++)
+    {
+        UnorderedMultimapNode *cur = map->buckets[i];
+        while (cur)
+        {
+            if (!first) printf(", ");
+            printf("%d: %d", cur->key, cur->value);
+            first = 0;
+            cur = cur->next;
+        }
+    }
+    printf("}\n");
+}
