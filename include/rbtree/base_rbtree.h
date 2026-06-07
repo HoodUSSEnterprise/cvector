90ab1b30703b8c3f3a5fd2d2e7f8447a99247c3d
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:47:56 +0800

    add comments to base_rbtree.h

--- include/rbtree/base_rbtree.h
+++ include/rbtree/base_rbtree.h
@@ -4,22 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 红黑树结点颜色 */
 typedef enum { RED, BLACK } Color;
 
+/* 红黑树结点 */
 typedef struct RBNode
 {
-    int key;
-    Color color;
-    struct RBNode *left;
-    struct RBNode *right;
-    struct RBNode *parent;
+    int key;                 /* 键值 */
+    Color color;             /* 结点颜色 */
+    struct RBNode *left;     /* 左孩子 */
+    struct RBNode *right;    /* 右孩子 */
+    struct RBNode *parent;   /* 父结点 */
 } RBNode;
 
+/* 红黑树 */
 typedef struct
 {
-    RBNode *root;
-    RBNode *nil;
-    int size;
+    RBNode *root;  /* 根结点 */
+    RBNode *nil;   /* 哨兵叶子结点（黑色） */
+    int size;      /* 结点数量 */
 } RBTree;
 
 #endif
