fbd6e0469206278586714ec08d768a3866d13996
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 20:32:11 +0800

    add unordered_multiset delete

---
+++ include/unordered_multiset/delete_unordered_multiset.h
@@ -0,0 +1,8 @@
+#ifndef DELETE_UNORDERED_MULTISET_H
+#define DELETE_UNORDERED_MULTISET_H
+
+#include "unordered_multiset/base_unordered_multiset.h"
+
+void unordered_multiset_delete(UnorderedMultiset *set, int key);
+
+#endif
---
+++ src/unordered_multiset/delete_unordered_multiset.c
@@ -0,0 +1,38 @@
+#include "unordered_multiset/delete_unordered_multiset.h"
+
+static int hash_key(int key, int capacity)
+{
+    return abs(key) % capacity;
+}
+
+void unordered_multiset_delete(UnorderedMultiset *set, int key)
+{
+    int idx = hash_key(key, set->capacity);
+
+    UnorderedMultisetNode *cur = set->buckets[idx];
+    UnorderedMultisetNode *prev = NULL;
+
+    while (cur)
+    {
+        if (cur->key == key)
+        {
+            if (cur->count > 1)
+            {
+                cur->count--;
+                set->size--;
+                return;
+            }
+
+            if (prev)
+                prev->next = cur->next;
+            else
+                set->buckets[idx] = cur->next;
+            free(cur);
+            set->size--;
+            set->unique--;
+            return;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+}
