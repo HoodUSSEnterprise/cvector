41d44f07b4adbcbad6cc8726632d5d990ad383aa
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-07 19:48:19 +0800

    add comments to print_rbtree.h

--- include/rbtree/print_rbtree.h
+++ include/rbtree/print_rbtree.h
@@ -3,8 +3,11 @@
 
 #include "rbtree/base_rbtree.h"
 
+/* 中序遍历打印（升序） */
 void rbtree_print_inorder(RBTree *tree);
+/* 前序遍历打印 */
 void rbtree_print_preorder(RBTree *tree);
+/* 后序遍历打印 */
 void rbtree_print_postorder(RBTree *tree);
 
 #endif
