c344f11bb550b87d35c975e62de17b5dba298c30
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-08 20:32:23 +0800

    fix:operator

--- include/stack/operator_stack.h
+++ include/stack/operator_stack.h
@@ -2,11 +2,38 @@
 #define OPERATOR_STACK_H
 
 #include "stack/base_stack.h"
+#include <stdbool.h>
 
-int top(Stack *stack, int *data);
+int top_int(StackInt *stack, int *data);
 
-int is_empty_stack(Stack *stack);
+float top_float(StackFloat *stack, float *data);
 
-int get_stack_size(Stack *stack);
+double top_double(StackDouble *stack, double *data);
 
+bool is_empty_stack_int(StackInt *stack);
+
+bool is_empty_stack_float(StackFloat *stack);
+
+bool is_empty_stack_double(StackDouble *stack);
+
+int get_stack_size_int(StackInt *stack);
+
+float get_stack_size_float(StackFloat *stack);
+
+double get_stack_size_double(StackDouble *stack);
+
+#define top(stack, data) _Generic((stack), \
+    StackInt *: top_int,                   \
+    StackFloat *: top_float,               \
+    StackDouble *: top_double)(stack, data)
+
+#define is_empty_stack(stack) _Generic((stack), \
+    StackInt *: is_empty_stack_int,             \
+    StackFloat *: is_empty_stack_float,         \
+    StackDouble *: is_empty_stack_double)(stack)
+
+#define get_stack_size(stack) _Generic((stack), \
+    StackInt *: get_stack_size_int,             \
+    StackFloat *: get_stack_size_float,         \
+    StackDouble *: get_stack_size_double)(stack)
 #endif // OPERATOR_STACK_H
--- src/stack/operator_stack.c
+++ src/stack/operator_stack.c
@@ -1,17 +1,58 @@
-#include "stack/base_stack.h"
-#include <stdbool.h>
+#include "stack/operator_stack.h"
 
-bool is_empty_stack(Stack *stack)
+bool is_empty_stack_int(StackInt *stack)
 {
     return (!stack || stack->size == 0) ? true : false;
 }
 
-int get_stack_size(Stack *stack)
+bool is_empty_stack_float(StackFloat *stack)
+{
+    return (!stack || stack->size == 0) ? true : false;
+}
+
+bool is_empty_stack_double(StackDouble *stack)
+{
+    return (!stack || stack->size == 0) ? true : false;
+}
+
+int get_stack_size_int(StackInt *stack)
 {
     return stack ? stack->size : 0;
 }
 
-int top(Stack *stack, int *data)
+float get_stack_size_float(StackFloat *stack)
+{
+    return stack ? stack->size : 0;
+}
+
+double get_stack_size_double(StackDouble *stack)
+{
+    return stack ? stack->size : 0;
+}
+
+int top_int(StackInt *stack, int *data)
+{
+    if (!stack || is_empty_stack(stack) || !data)
+    {
+        return -1;
+    }
+
+    *data = stack->top->data;
+    return 0;
+}
+
+float top_float(StackFloat *stack, float *data)
+{
+    if (!stack || is_empty_stack(stack) || !data)
+    {
+        return -1;
+    }
+
+    *data = stack->top->data;
+    return 0;
+}
+
+double top_double(StackDouble *stack, double *data)
 {
     if (!stack || is_empty_stack(stack) || !data)
     {
