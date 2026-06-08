947b3ad508b13458b6b0e014790d2c314e19a3d1
HoodUSSEnterprise <haoyangh114@gmail.com>
2026-06-08 19:38:28 +0800

    format

--- visualize/main.c
+++ visualize/main.c
@@ -18,18 +18,19 @@
 #define MAX_VISIBLE 10
 
 #define BTN_PUSH_X 400
-#define BTN_POP_X  400
-#define BTN_CLR_X  400
-#define BTN_W      140
-#define BTN_H      44
-#define BTN_GAP    14
+#define BTN_POP_X 400
+#define BTN_CLR_X 400
+#define BTN_W 140
+#define BTN_H 44
+#define BTN_GAP 14
 #define BTN_Y_START 80
 
 #ifdef USE_SDL_TTF
 static TTF_Font *font = NULL;
 #endif
 
-typedef struct {
+typedef struct
+{
     SDL_Window *window;
     SDL_Renderer *renderer;
     Stack *stack;
@@ -38,13 +39,13 @@ typedef struct {
     int status_timer;
 } App;
 
-static SDL_Color color_bg   = { 30, 30, 40, 255 };
-static SDL_Color color_box  = { 60, 100, 180, 255 };
-static SDL_Color color_top  = { 200, 80, 60, 255 };
-static SDL_Color color_btn  = { 70, 70, 90, 255 };
-static SDL_Color color_btn_hover = { 100, 100, 130, 255 };
-static SDL_Color color_text = { 220, 220, 230, 255 };
-static SDL_Color color_label = { 180, 180, 200, 255 };
+static SDL_Color color_bg = {30, 30, 40, 255};
+static SDL_Color color_box = {60, 100, 180, 255};
+static SDL_Color color_top = {200, 80, 60, 255};
+static SDL_Color color_btn = {70, 70, 90, 255};
+static SDL_Color color_btn_hover = {100, 100, 130, 255};
+static SDL_Color color_text = {220, 220, 230, 255};
+static SDL_Color color_label = {180, 180, 200, 255};
 
 static int init_app(App *app)
 {
@@ -76,9 +77,18 @@ static int init_app(App *app)
 
 #ifdef USE_SDL_TTF
     font = TTF_OpenFont("C:/Windows/Fonts/arial.ttf", 20);
-    if (!font) font = TTF_OpenFont("C:/Windows/Fonts/msyh.ttc", 20);
-    if (!font) font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20);
-    if (!font) fprintf(stderr, "TTF_OpenFont: %s\n", SDL_GetError());
+    if (!font)
+    {
+        font = TTF_OpenFont("C:/Windows/Fonts/msyh.ttc", 20);
+    }
+    if (!font)
+    {
+        font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20);
+    }
+    if (!font)
+    {
+        fprintf(stderr, "TTF_OpenFont: %s\n", SDL_GetError());
+    }
 #endif
 
     srand((unsigned)time(NULL));
@@ -88,12 +98,24 @@ static int init_app(App *app)
 static void close_app(App *app)
 {
 #ifdef USE_SDL_TTF
-    if (font) TTF_CloseFont(font);
+    if (font)
+    {
+        TTF_CloseFont(font);
+    }
     TTF_Quit();
 #endif
-    if (app->stack) free_stack(app->stack);
-    if (app->renderer) SDL_DestroyRenderer(app->renderer);
-    if (app->window) SDL_DestroyWindow(app->window);
+    if (app->stack)
+    {
+        free_stack(app->stack);
+    }
+    if (app->renderer)
+    {
+        SDL_DestroyRenderer(app->renderer);
+    }
+    if (app->window)
+    {
+        SDL_DestroyWindow(app->window);
+    }
     SDL_Quit();
 }
 
@@ -105,13 +127,19 @@ static int is_hover(int mx, int my, int rx, int ry, int rw, int rh)
 #ifdef USE_SDL_TTF
 static void render_text(SDL_Renderer *renderer, const char *text, int x, int y, SDL_Color color)
 {
-    if (!font) return;
+    if (!font)
+    {
+        return;
+    }
     SDL_Surface *surf = TTF_RenderText_Blended(font, text, SDL_strlen(text), color);
-    if (!surf) return;
+    if (!surf)
+    {
+        return;
+    }
     SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, surf);
     if (tex)
     {
-        SDL_FRect dst = { (float)x, (float)y, (float)surf->w, (float)surf->h };
+        SDL_FRect dst = {(float)x, (float)y, (float)surf->w, (float)surf->h};
         SDL_RenderTexture(renderer, tex, NULL, &dst);
         SDL_DestroyTexture(tex);
     }
@@ -120,13 +148,17 @@ static void render_text(SDL_Renderer *renderer, const char *text, int x, int y, 
 #else
 static void render_text(SDL_Renderer *renderer, const char *text, int x, int y, SDL_Color color)
 {
-    (void)renderer; (void)text; (void)x; (void)y; (void)color;
+    (void)renderer;
+    (void)text;
+    (void)x;
+    (void)y;
+    (void)color;
 }
 #endif
 
 static void draw_button(SDL_Renderer *renderer, const char *label, int x, int y, int w, int h, int hover)
 {
-    SDL_FRect rect = { (float)x, (float)y, (float)w, (float)h };
+    SDL_FRect rect = {(float)x, (float)y, (float)w, (float)h};
     SDL_SetRenderDrawColor(renderer, hover ? color_btn_hover.r : color_btn.r,
                            hover ? color_btn_hover.g : color_btn.g,
                            hover ? color_btn_hover.b : color_btn.b, 255);
@@ -138,18 +170,22 @@ static void draw_button(SDL_Renderer *renderer, const char *label, int x, int y,
 
 static void draw_number_box(SDL_Renderer *renderer, int value, int x, int y, int is_top)
 {
-    SDL_FRect rect = { (float)x, (float)y, BOX_W, BOX_H };
+    SDL_FRect rect = {(float)x, (float)y, BOX_W, BOX_H};
     if (is_top)
+    {
         SDL_SetRenderDrawColor(renderer, color_top.r, color_top.g, color_top.b, 255);
+    }
     else
+    {
         SDL_SetRenderDrawColor(renderer, color_box.r, color_box.g, color_box.b, 255);
+    }
     SDL_RenderFillRect(renderer, &rect);
     SDL_SetRenderDrawColor(renderer, color_label.r, color_label.g, color_label.b, 255);
     SDL_RenderRect(renderer, &rect);
 
     char buf[16];
     snprintf(buf, sizeof(buf), "%d", value);
-    render_text(renderer, buf, x + BOX_W/2 - 12, y + 10, color_text);
+    render_text(renderer, buf, x + BOX_W / 2 - 12, y + 10, color_text);
 }
 
 static void render(App *app)
@@ -172,9 +208,12 @@ static void render(App *app)
     {
         int idx = count - 1 - i; /* bottom of visual = last in array */
         int vy = STACK_BASE_Y - (i + 1) * (BOX_H + BOX_GAP);
-        if (vy < STACK_TOP_Y) break;
+        if (vy < STACK_TOP_Y)
+        {
+            break;
+        }
         draw_number_box(app->renderer, values[idx],
-                        STACK_CENTER_X - BOX_W/2, vy,
+                        STACK_CENTER_X - BOX_W / 2, vy,
                         i == 0);
     }
 
@@ -182,7 +221,7 @@ static void render(App *app)
     if (count > 0)
     {
         int vy = STACK_BASE_Y - 1 * (BOX_H + BOX_GAP);
-        render_text(app->renderer, "TOP", STACK_CENTER_X + BOX_W/2 + 10, vy + 10, color_top);
+        render_text(app->renderer, "TOP", STACK_CENTER_X + BOX_W / 2 + 10, vy + 10, color_top);
     }
 
     /* Draw title */
@@ -263,9 +302,13 @@ static void handle_click(App *app, int mx, int my)
 
 int main(int argc, char *argv[])
 {
-    (void)argc; (void)argv;
+    (void)argc;
+    (void)argv;
     App app;
-    if (!init_app(&app)) return 1;
+    if (!init_app(&app))
+    {
+        return 1;
+    }
 
     while (app.running)
     {
@@ -273,13 +316,19 @@ int main(int argc, char *argv[])
         while (SDL_PollEvent(&e))
         {
             if (e.type == SDL_EVENT_QUIT)
+            {
                 app.running = 0;
+            }
             else if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+            {
                 handle_click(&app, (int)e.button.x, (int)e.button.y);
+            }
             else if (e.type == SDL_EVENT_KEY_DOWN)
             {
                 if (e.key.key == SDLK_ESCAPE)
+                {
                     app.running = 0;
+                }
             }
         }
         render(&app);
